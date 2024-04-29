8f2b41c4901e7ffc55a66efc834f5bb5930374fa
Felipe <feliperos.simao@gmail.com>
2024-04-29 18:37:08 -0300

    refactor(data): new approach for data container.

--- src/main/java/fun/felipe/powerfulbackpacks/utils/items/PersistentDataUtils.java
+++ src/main/java/fun/felipe/powerfulbackpacks/utils/items/PersistentDataUtils.java
@@ -3,26 +3,25 @@ package fun.felipe.powerfulbackpacks.utils.items;
 import fun.felipe.powerfulbackpacks.PowerfulBackpacks;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BundleMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
 public class PersistentDataUtils {
 
-    public static ItemStack addStringData(ItemStack itemStack, String key, String data) {
-        BundleMeta bundleMeta = (BundleMeta) itemStack.getItemMeta();
-        bundleMeta.getPersistentDataContainer().set(buildKey(key), PersistentDataType.STRING, data);
-        itemStack.setItemMeta(bundleMeta);
-        return itemStack;
+    public static void addStringData(ItemStack itemStack, String key, String data) {
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.getPersistentDataContainer().set(buildKey(key), PersistentDataType.STRING, data);
+        itemStack.setItemMeta(meta);
     }
 
     public static boolean hasData(ItemStack itemStack, String key) {
-        BundleMeta bundleMeta = (BundleMeta) itemStack.getItemMeta();
-        return bundleMeta.getPersistentDataContainer().has(buildKey(key));
+        ItemMeta meta = itemStack.getItemMeta();
+        return meta.getPersistentDataContainer().has(buildKey(key));
     }
 
     public static String getStringData(ItemStack itemStack, String key) {
-        BundleMeta bundleMeta = (BundleMeta) itemStack.getItemMeta();
-        return bundleMeta.getPersistentDataContainer().get(buildKey(key), PersistentDataType.STRING);
+        ItemMeta meta = itemStack.getItemMeta();
+        return meta.getPersistentDataContainer().get(buildKey(key), PersistentDataType.STRING);
     }
 
     public static NamespacedKey buildKey(String key) {
