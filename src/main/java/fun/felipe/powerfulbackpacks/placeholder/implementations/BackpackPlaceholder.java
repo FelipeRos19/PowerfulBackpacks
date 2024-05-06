daab588aaa7bd6a1d2cb6463ea881b81679019c9
Felipe <feliperos.simao@gmail.com>
2024-05-06 16:59:30 -0300

    refactor(placeholder): new approach for placeholders.

---
+++ src/main/java/fun/felipe/powerfulbackpacks/placeholder/implementations/BackpackPlaceholder.java
@@ -0,0 +1,10 @@
+package fun.felipe.powerfulbackpacks.placeholder.implementations;
+
+import fun.felipe.powerfulbackpacks.entities.BackpackEntity;
+import fun.felipe.powerfulbackpacks.placeholder.Placeholder;
+
+public class BackpackPlaceholder extends Placeholder<BackpackEntity> {
+    public BackpackPlaceholder(BackpackEntity backpack) {
+        super(backpack);
+    }
+}
---
+++ src/main/java/fun/felipe/powerfulbackpacks/placeholder/implementations/MessagePlaceholder.java
@@ -0,0 +1,9 @@
+package fun.felipe.powerfulbackpacks.placeholder.implementations;
+
+import fun.felipe.powerfulbackpacks.placeholder.Placeholder;
+
+public class MessagePlaceholder extends Placeholder<String> {
+    public MessagePlaceholder(String placeholder) {
+        super(placeholder);
+    }
+}
