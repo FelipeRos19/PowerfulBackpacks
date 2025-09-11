1d5edf46db283f1e2f9c2979c4c16403173ca668
FelipeRos19 <feliperos.simao@gmail.com>
2025-09-11 12:14:15 -0300

    feat(items): new approach for items load system. (#16)

--- src/main/java/fun/felipe/powerfulbackpacks/managers/items/ItemManager.java
+++ src/main/java/fun/felipe/powerfulbackpacks/managers/items/ItemManager.java
@@ -2,49 +2,63 @@ package fun.felipe.powerfulbackpacks.managers.items;
 
 import fun.felipe.powerfulbackpacks.PowerfulBackpacks;
 import fun.felipe.powerfulbackpacks.managers.items.entities.LoadItemConfig;
+import fun.felipe.powerfulbackpacks.managers.items.implementation.SewingKitItem;
+import fun.felipe.powerfulbackpacks.managers.items.interfaces.GenericItem;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class ItemManager {
     private final PowerfulBackpacks plugin;
+    private final Map<String, LoadItemConfig> registeredItems;
 
     public ItemManager(PowerfulBackpacks plugin) {
         this.plugin = plugin;
+        this.registeredItems = new HashMap<>();
     }
 
     private void loadItems() {
-
+        Optional<LoadItemConfig> sewingKit = this.loadItemFromConfig("sewing_kit");
+        sewingKit.ifPresent(config -> {
+            this.registeredItems.put("sewing_kit", config);
+        });
     }
 
-    private LoadItemConfig loadItem(String itemKey) {
+    @NotNull
+    private Optional<LoadItemConfig> loadItemFromConfig(String itemKey) {
         ConfigurationSection itemsSection = this.plugin.getItemFile().getFileConfiguration().getConfigurationSection("Items." + itemKey);
         if (itemsSection == null) {
             this.plugin.getLogger().severe("Item file not found in Config!");
-            return null;
+            return Optional.empty();
         }
 
         String itemName = itemsSection.getString("name");
+        if (itemName == null) {
+            this.plugin.getLogger().severe("Item name not found in Config!");
+            return Optional.empty();
+        }
         List<String> itemLore =  itemsSection.getStringList("lore");
         String materialString = itemsSection.getString("material");
         if (materialString == null) {
             this.plugin.getLogger().severe("Material not found in Config! (" + itemKey + ")");
-            return null;
+            return Optional.empty();
         }
 
         Material material =  Material.getMaterial(materialString);
         if (material == null) {
             this.plugin.getLogger().severe("Material invalid in Config! (" + itemKey + ")");
-            return null;
+            return Optional.empty();
         }
 
         ConfigurationSection craftSection = itemsSection.getConfigurationSection("craft");
         if (craftSection == null) {
             this.plugin.getLogger().severe("Craft section not found in Config!");
-            return null;
+            return Optional.empty();
         }
 
         List<String> craftShape = craftSection.getStringList("shape");
@@ -58,13 +72,13 @@ public class ItemManager {
 
                     if (materialStringKey == null) {
                         this.plugin.getLogger().severe("Material key not found in Config! (" + key + ")");
-                        return null;
+                        return Optional.empty();
                     }
 
                     Material materialResult = Material.getMaterial(materialStringKey);
                     if (materialResult == null) {
                         this.plugin.getLogger().severe("Material invalid in Config! (" + key + ")");
-                        return null;
+                        return Optional.empty();
                     }
 
                     craftMaterials.put(key.charAt(0), materialResult);
@@ -72,6 +86,6 @@ public class ItemManager {
             }
         }
 
-        return new LoadItemConfig(itemName, itemLore, material, craftShape, craftMaterials);
+        return Optional.of(new LoadItemConfig(itemName, itemLore, material, craftShape, craftMaterials));
     }
 }
